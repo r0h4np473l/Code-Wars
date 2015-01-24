39230b9dfa6cc805017fd43566ab9fd36ecf3225
dgandhi98 <Divyanshu@Divyanshus-MacBook-Pro.local>
2015-01-24 12:41:29 -0500

    Problem 13 Complete
    Used some efficient code online from:
    http://www.dotnetperls.com/remove-duplicates-arraylist-java
    http://learnprogramming.machinesentience.com/java_permutations_recursion
    /

---
+++ Divyanshu/CodeWars2014Solutions/y2014p13.java
@@ -0,0 +1,91 @@
+package CodeWars2014Solutions;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class y2014p13 {
+	private static ArrayList<String> allPerms = new ArrayList<String>();
+	//Recursive Code taken from http://learnprogramming.machinesentience.com/java_permutations_recursion/
+	private static void permutation(String prefix, String str){
+		int n = str.length();
+		if (n == 0){
+			//System.out.println(prefix);
+			allPerms.add(prefix);
+		}
+		else {
+			for (int i = 0; i < n; i++)
+				permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1));
+		}
+	}
+	//Code Taken From http://www.dotnetperls.com/remove-duplicates-arraylist-java
+    static ArrayList<String> removeDuplicates(ArrayList<String> list) {
+	// Store unique items in result
+	ArrayList<String> result = new ArrayList<>();
+	// Record encountered Strings in HashSet.
+	HashSet<String> set = new HashSet<>();
+	// Loop over argument list.
+	for (String item : list) {
+	    // If String is not in set, add it to the list and the set.
+	    if (!set.contains(item)) {
+		result.add(item);
+		set.add(item);
+	    }
+	}
+	return result;
+    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader f =new BufferedReader(new FileReader("y2014p13.txt"));
+		int numLet = Integer.parseInt(f.readLine());
+		String line = f.readLine();
+		StringTokenizer st= new StringTokenizer(line);
+		String[] letters = new String[numLet];
+		int i=0;
+		//creates the letter array of the letters given
+		while(st.hasMoreTokens()){
+			letters[i] = st.nextToken();
+			i++;
+		}
+		//System.out.println(Arrays.toString(letters));
+		//creates combinations of 5 for all in letter array, making sure none repeat
+		for(i=0;i<numLet;i++){
+			for(int j=1;j<numLet;j++){
+				if(i!=j)
+				for(int k=2;k<numLet;k++){
+					if(i!=k&&j!=k)
+					for(int l=3;l<numLet;l++){
+						if(i!=l&&j!=l&&k!=l)
+						for(int m=4;m<numLet;m++){
+							if(i!=m&&j!=m&&k!=m&&l!=m)
+							permutation("", letters[i]+letters[j]+letters[k]+letters[l]+letters[m]);
+		}}}}}
+		//sorts
+		Collections.sort(allPerms);
+		//removes duplicates
+		allPerms = removeDuplicates(allPerms);
+		
+		//System.out.println(allPerms.size());
+		//System.out.println(allPerms.toString());
+		
+		//Iterates through terms of K
+		int index=0;
+		while(((line=f.readLine())!=null) && !line.equals("0")){
+			//Prints Results
+			index = Integer.parseInt(line)-1;
+			try{
+			System.out.println(line+":"+allPerms.get(index));
+			}
+			catch(IndexOutOfBoundsException e){
+				System.out.println("list length: " +allPerms.size()+"\tlast"+allPerms.get(allPerms.size()-1));
+			}
+		}
+		//exit
+		System.exit(0);
+	}
+}
