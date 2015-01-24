21af2ce4d4e3fcf093f366a2b39e31922d77bcdd
r0h4np473l <r0h4np473l@gmail.com>
2015-01-24 15:47:33 -0500

    Working on problem 10

--- Rohan/Solutions2014/y2014p10.java
+++ Rohan/Solutions2014/y2014p10.java
@@ -7,7 +7,8 @@ public class y2014p10 {
 
 	static char[] alphabet={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 	static char[] input;
-	static ArrayList<Character> outputSeparated= new ArrayList<>();
+	static String decodedString="t";
+	static int pointerIndex=0;
 	public static void main(String[] args) 
 	{
 		//HELLO
@@ -15,48 +16,21 @@ public class y2014p10 {
 		String line=scan.nextLine();
 		input=new char[line.length()];
 		line.getChars(0, line.length(), input, 0);
-		for(int j=0; j<line.length(); j++)
+		decodedString=decodedString + input[0];
+		for(int i=1; i<line.length()-1; i++)
 		{
-			for(int i=0; i<alphabet.length; i++)
+			for (int j=0;j<26; j++)
 			{
-				if(alphabet[i]==input[j])
+				System.out.println("running...for loop");
+				if(alphabet[j]==input[i])
 				{
-					if(outputSeparated.get(j)==input[j])
-					{
-						outputSeparated=shiftArrayList(outputSeparated, j);
-					}
-					else
-					{
-						outputSeparated.set(j, alphabet[i]);
-					}
+					System.out.println("running...if loop");
+					decodedString=decodedString.substring(0,i) + alphabet[j] + decodedString.substring(i++);
 				}
-				else
-				{
-					outputSeparated.add(input[j]);
-				}
-				i++;
 			}
 		}
-
-		System.out.println(outputSeparated);
+		System.out.println(decodedString);
 	}
-	public static ArrayList<Character> shiftArrayList(ArrayList<Character> arraylist, int index)
-	{
-		ArrayList <Character> temporaryArrayList = new ArrayList<>();
-		int iAdd=index+=1;
-		for(int i=index; i< arraylist.size(); i++){
-			if(temporaryArrayList.size()<iAdd)
-			{
-				temporaryArrayList.add(arraylist.get(i));
-			}
-			else
-			{
-				temporaryArrayList.set(iAdd, arraylist.get(i));
-			}
-			iAdd++;
-
-		}
-		return temporaryArrayList;
-	}
+	
 
 }
