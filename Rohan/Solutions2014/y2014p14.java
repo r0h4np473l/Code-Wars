6d0f1e40175e2b8fe0f16b727c0617083e3e3b72
r0h4np473l <r0h4np473l@gmail.com>
2015-01-26 22:10:12 -0500

    Worked on problem 14 and added solutions checklist
    worked on problem 14, still don’t know how to do it. Added solutions
    checklist, to keep track of which questions were solved and which
    questions were not.

--- Rohan/Solutions2014/y2014p14.java
+++ Rohan/Solutions2014/y2014p14.java
@@ -7,59 +7,24 @@ public class y2014p14 {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		//Input
 		Scanner input= new Scanner(System.in);
-		int largestNumber=0;
-		int highestPossibleNumber=0;
-		String inputLine=input.nextLine();
-		int numOfWeights=Integer.parseInt(inputLine.substring(0,1));
-		System.out.println(numOfWeights);
-		inputLine=inputLine + " ";
-		ArrayList<Integer> numbers = new ArrayList<>();
+		int largestNumber=0; //initialize variables
+		int highestPossibleNumber=0; //initialize variables
+		String inputLine=input.nextLine(); //read in input
+		int numOfWeights=Integer.parseInt(inputLine.substring(0,1)); //get the number of weights
+		System.out.println(numOfWeights); //debug
+		inputLine=inputLine + " "; //used to manipulate string
+		ArrayList<Integer> numbers = new ArrayList<>(); //stores the weights
+		
+		//adds the weights into the arraylist
 		for (int k=2; k<inputLine.length(); k+=1)
 		{
 			numbers.add(new Integer(inputLine.substring(k,k+=1)));
 		}
 		System.out.println(numbers);
+		
 
-		//2 digits
-		int i;
-		if(numOfWeights==2)
-		{
-			highestPossibleNumber=numbers.get(0) * numbers.get(1);
-			for(i=1; i<highestPossibleNumber; i++)
-			{
-
-				System.out.println("running for loop");//debug
-					while(i%numbers.get(0)==0)
-					{
-						i=i/numbers.get(0);
-						System.out.println("running first while" +  "i value" + i);//debug
-					}
-					while(i%numbers.get(1)==0)
-					{
-						System.out.println("i" + i);
-						System.out.println(i%numbers.get(1));
-						i=i/numbers.get(1); //(1)
-						System.out.println("running second while" + "i value" + i);//debug
-					}//gets stuck in these while loops, need to figure out a better way of ending them. gets stuck in infinite loop.
-					// at (1) the value is changed to 5/5 =1 and then its never saved because after the while loop is done its changed back.
-			}
-			System.out.println(i); //debug
-				
-		}
-		//3 digits
-		if(numOfWeights==3)
-		{	
-			
-		}
-		//4 digits
-		if(numOfWeights==4)
-		{
-			
-		}
-	
-		System.out.println("Largest number" + largestNumber); //ignore, not fully working yet. 
 	}
 	
 
@@ -70,5 +35,5 @@ public class y2014p14 {
  * read in the strings. 
  * next isolate the first value. store that in a variable. 
  * next read in the other two values and store them in their own variables
- * start attempting to divide a number by the two other numbers
+ * start attempting to divide a number by each number until there is a remainder, then move onto the next number
  */
