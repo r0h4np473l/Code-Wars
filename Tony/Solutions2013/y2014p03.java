74b46caeca093eeef99b6fac8c0d4912c072de07
ton3 <tony1004wu@yahoo.com>
2015-01-28 23:55:24 -0500

    Problem 3
    Skipped problem 2, it was too hard. Started problem 3, and the
    instantiation of some of it, will do more after i finish my sat prep

---
+++ Tony/Solutions2013/y2014p03.java
@@ -0,0 +1,17 @@
+package Solutions2013;
+
+import java.util.Scanner;
+
+public class y2014p03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int hour = input.nextInt();
+		int minute = input.nextInt();
+		int divider = 20;
+		
+		
+	}
+
+}
