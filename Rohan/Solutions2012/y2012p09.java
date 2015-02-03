8e9b5a434da023950dd4b6531fc989282bc700a1
dgandhi98 <Divyanshu@Divyanshus-MacBook-Pro.local>
2015-02-03 00:52:19 -0500

    Fixed Rohan's Problem 9

--- Rohan/Solutions2012/y2012p09.java
+++ Rohan/Solutions2012/y2012p09.java
@@ -1,27 +1,34 @@
 package Solutions2012;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class y2012p09 {
 
-	static char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-		'X', 'Y', 'Z' };
 
-	static char[] finalString;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+				'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+				'X', 'Y', 'Z' };
 
+		char[] finalString;
 		// Input
 		Scanner input = new Scanner(System.in);
 		String inputString = input.nextLine();
-
+		//BufferedReader input = new BufferedReader(new FileReader("y2012p09.txt"));
+		//String inputString1 = input.readLine();
+		//String inputString = input.readLine();
 		int indexOfKey = inputString.indexOf("KEY ");
 		int indexOfDecodingString = indexOfKey += 4;
 
 		String decodingString = inputString.substring(indexOfDecodingString);
-		int shiftSize = decodingString.length();
+		int shiftSize = 4;	//decodingString.length();
 
 		// Search and replace
 		String message = inputString.substring(0, indexOfKey-=4);
@@ -29,36 +36,46 @@ public class y2012p09 {
 		finalString = new char[messageLength];
 		message.getChars(0, indexOfKey, finalString, 0);
 		int newLetterIndex;
+		Boolean foundCharacter=false;
 		for (int i = 0; i < messageLength; i++) 
 		{
-			for (int j = 0; j < 26; j++) 
+			if(finalString[i]!=' ')
 			{
-				if (finalString[i] == letters[j]) 
+				foundCharacter=false;
+				int j=0;
+				//for (int j = 0; j < 26; j++) <== This is what was wrong
+				while(j<26 && !foundCharacter)
 				{
-					int jMutable=j;
-					int letterIndex = jMutable -= shiftSize;
+					if (finalString[i] == letters[j] ) 
+					{
+						//int jMutable=j;
+						int letterIndex = j - shiftSize;
 
-					if (letterIndex < 0) 
-					{
-						newLetterIndex = (int) (26.0 - Math.abs((double) letterIndex));
-					} 
-					else 
-					{
-						newLetterIndex = letterIndex;
+						if (letterIndex < 0) 
+						{
+							//newLetterIndex = (int) (26.0 - Math.abs((double) letterIndex));
+							newLetterIndex = (letterIndex)+26;
+						} 
+						else 
+						{
+							newLetterIndex = letterIndex;
+						}
+						finalString[i] = letters[newLetterIndex];
+						foundCharacter=true;
 					}
-
-					finalString[i] = letters[newLetterIndex];
+					j++;
 				}
 			}
 		}
-	
+
 		//Print out
+		//System.out.println(Arrays.toString(finalString));
 		
 		for(int i=0; i<finalString.length; i++)
 		{
 			System.out.print(finalString[i]);
 		}
-	
+
 	}
 
 }
