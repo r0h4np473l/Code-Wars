b598f982bf39aded302d5f2b5079654c65c1d876
dgandhi98 <Divyanshu@Divyanshus-MacBook-Pro.local>
2015-02-02 23:54:00 -0500

    commented problem 9

--- Divyanshu/CodeWars2012Solutions/y2012p09.java
+++ Divyanshu/CodeWars2012Solutions/y2012p09.java
@@ -8,34 +8,52 @@ import java.util.StringTokenizer;
 public class y2012p09 {
 
 	public static void main(String[] args) throws IOException {
+		//import lines
 		BufferedReader f = new BufferedReader(new FileReader("y2012p09.txt"));
 		String all="";
 		String line="";
-		all=f.readLine();
-		line = f.readLine();
+		all=f.readLine();		//cipher
+		line = f.readLine();	//decoded message
 		StringTokenizer st = new StringTokenizer(line);
+		
+		//idk what 'KEY' has to do with anything, just take the decoded word
 		String decoded = st.nextToken();
 		decoded=st.nextToken();
+		
+		//While we haven't found the correct shift, I simply tested every decoded cipher possibility
+		//until it hit the correct one. 
 		Boolean checkDecode=true;
 		int x=1;
 		String tempDecipher="";
 		int indexOfDecoded=0;
-		while(checkDecode){
-			indexOfDecoded=0;
+		while(checkDecode)
+		{
+			indexOfDecoded=0;	//iterate through decoded
+			int i=0;			//iterate through decipher
+			//get the temporary cipher to test, changes with every iteration
 			tempDecipher = decipherCaesarShift(all,x,'L');
-			int i=0;
-			while(indexOfDecoded<decoded.length() && i<tempDecipher.length()){
-				if(tempDecipher.charAt(i)==decoded.charAt(indexOfDecoded)){
+			
+			//iterate through the decipher
+			//if a letter matches, increment the index of decoded message
+			//keep checking until either you hit the end of the decipher or decoded message
+			//if you hit the end of the decoded, then that means you found the correct shift
+			while(indexOfDecoded<decoded.length() && i<tempDecipher.length())
+			{
+				if(tempDecipher.charAt(i)==decoded.charAt(indexOfDecoded))
+				{
 					indexOfDecoded++;	
 				}
 				i++;
 			}
-			if(indexOfDecoded==decoded.length()){
+			if(indexOfDecoded==decoded.length())
+			{
 				checkDecode=false;
 			}
 			x++;
 		}
+		//print
 		System.out.println(tempDecipher);
+		//exit program
 		System.exit(0);
 	}
 	public static String decipherCaesarShift(String s, int shift, char dir)
