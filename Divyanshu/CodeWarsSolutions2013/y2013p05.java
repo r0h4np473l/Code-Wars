7f61f270d6f43c676120cf2fadba914a3c2dfeb6
dgandhi98 <dgandhi98@users.noreply.github.com>
2015-02-05 14:33:26 -0500

    year 2013 problem 5
    I was bored in laufers, apparently you can install eclipse and github in school now (i uploaded this through web thoooooo)

---
+++ Divyanshu/CodeWarsSolutions2013/y2013p05.java
@@ -0,0 +1,34 @@
+package CodeWarsSolution2013;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+//'Tribonacci' sequence
+public class problem {
+	public static void main(String[] args) throws IOException{
+		BufferedReader f = new BufferedReader(new FileReader(("y2013p05.txt")));
+		int[] trib = new int[30];
+		trib[0] = 0;
+		trib[1]=1;	
+		trib[2]=1;
+		for(int i =3;i<30;i++){
+			trib[i]=trib[i-3]+trib[i-2]+trib[i-1];
+		}
+		Scanner in = new Scanner(System.in);
+		String line = "";
+		int userindex = 0;
+		while((line = f.readLine())!=null){
+			userindex=Integer.parseInt(line);
+			try{
+			System.out.println(trib[userindex]);
+			}
+			catch(IndexOutOfBoundsException e){
+				System.out.println(0);
+				break;
+			}
+		}
+		
+	}
+}
