5bec68c1bba78e65648090ef5d5ccafa4d0f0a14
dgandhi98 <Divyanshu@Divyanshus-MacBook-Pro.local>
2015-03-02 16:07:57 -0500

    worked on round up 5 question for 2013 problem 7
    swiggity swag shfifty five

---
+++ Divyanshu/CodeWars2013Solutions/y2013p07.java
@@ -0,0 +1,46 @@
+package CodeWars2013Solutions;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class y2013p07 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader f = new BufferedReader(new FileReader("y2013p07.txt"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("outfile.txt")));
+		String line="";
+		StringTokenizer st = new StringTokenizer(line);
+		int h=0;
+		while(!(line=f.readLine()).equals("-1")){
+			int min =Integer.parseInt(line);
+			int min2 =(int) (5*(Math.ceil(Math.abs(min/5.0))));
+			int dist = Math.abs(30-min2);
+			if(min/5<6){
+				h=6+dist/5;
+			}
+			else if(min/5>6){
+				h=6-dist/5;
+			}
+			if((""+min2).length()==1){
+				System.out.println(h+":0"+min);
+			}
+			else {
+				System.out.println(h+":"+min);
+			}	
+		}
+		f.close();
+		out.close();
+		System.exit(0);
+	}
+	public static double round(int x,int s){
+		double l =x;
+		l=(int)(l*Math.pow(10,s));
+		l=l/Math.pow(10,s);
+		return l;
+	}
+}
