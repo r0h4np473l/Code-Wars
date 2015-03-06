d997c8915ac01e4a64b90ce55b1fbf48e6e6d5a2
ton3 <tony1004wu@yahoo.com>
2015-03-06 15:53:26 -0500

    changes

--- Divyanshu/solution2011/y2011p07.java
+++ Divyanshu/solution2011/y2011p07.java
@@ -6,8 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class y2011p07 {
 
@@ -20,7 +18,6 @@ public class y2011p07 {
 		
 		while(!(line=f.readLine()).equals(".")){
 		}
-
 		
 		f.close();
 		out.close();
